475ba1561a7074f132d88e8da543c635cbd10a3a
progin <88029537+milmip@users.noreply.github.com>
2024-04-01 10:47:27 +0200

    Update infos.py

--- source/infos.py
+++ source/infos.py
@@ -1,15 +1,15 @@
 class DocumentInfos:
 
-    title = u'Titre de votre travail personnel (modifier dans `source/infos.py`)'
-    first_name = 'Prénom (infos.py)'
-    last_name = 'Nom de famille (infos.py)'
+    title = u'Flight Viewer'
+    first_name = 'Emilien'
+    last_name = 'Progin'
     author = f'{first_name} {last_name}'
     year = u'2024'
-    month = u'Janvier'
+    month = u'avril'
     seminary_title = u'Projet OCI'
     tutor = u"Cédric Donner"
     release = "(Version finale)"
-    repository_url = "https://github.com/<username>/<reponame>"
+    repository_url = "https://github.com/milmip/<reponame>"
 
     @classmethod
     def date(cls):
